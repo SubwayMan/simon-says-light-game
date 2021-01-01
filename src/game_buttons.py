cd905cd23d150fcd46a59dbc9924a540dd99dd7e
SubwayMan <tyche4490@gmail.com>
2021-01-01 01:30:44 -0800

    working sprite change state for colorbutton

--- src/game_buttons.py
+++ src/game_buttons.py
@@ -7,6 +7,7 @@ class Simon_Button(Fl_Button):
 
     def __init__(self, x, y, rad):
 
+        self.rad = rad
         Fl_Button.__init__(self, x, y, rad*2, rad*2)
         self.spritedir = os.path.join(os.getcwd(), "..", "assets")
         self.box(FL_NO_BOX)
@@ -19,8 +20,9 @@ class Simon_Button(Fl_Button):
 
     def click_cb(self, n):
 
-        cx = Fl.event_x() - self.rad
-        cy = Fl.event_y() - self.rad
+        mx, my = Fl.event_x(), Fl.event_y()
+        cx = mx - self.rad
+        cy = my - self.rad
         print(cx, cy)
         crad = math.pi * math.pow(self.rad, 2)
         mdist = math.pow(cx, 2) + math.pow(cy, 2)
@@ -28,12 +30,26 @@ class Simon_Button(Fl_Button):
         if math.sqrt(mdist) > self.rad:
             print("exit")
             return None
-        
+
+        if mx < self.rad and my <= self.rad:
+            self.chcol("greenlight.png")
+        elif mx >= self.rad and my < self.rad:
+            self.chcol("redlight.png")
+        elif mx > self.rad and my >= self.rad:
+            self.chcol("bluelight.png")
+        elif mx <= self.rad and my > self.rad:
+            self.chcol("yellight.png")
+
+    def chcol(self, spr):
+
+        self.sprite = Fl_PNG_Image(os.path.join(self.spritedir, spr))
+        self.image(self.sprite.copy(self.rad*2, self.rad*2))
+        self.redraw()
 
 
 if __name__ == "__main__":
 
-    win = Fl_Window(500, 500, "buttontest")
+    win = Fl_Double_Window(500, 500, "buttontest")
     win.begin()
     
     b = Simon_Button(0, 0, 250)
